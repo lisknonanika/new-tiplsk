38f8e103b6332377cd39987894938d76d437e788
lisknonanika <lisknonanika@gmail.com>
2021-06-09 09:47:19 +0900

    change name

---
+++ test/index.js
@@ -0,0 +1,17 @@
+const registration = require('../bot/dist/command/registration');
+const apiClient = require('@liskhq/lisk-api-client');
+const { RPC_ENDPOINT } = require('../bot/dist/const');
+
+(async ()=> {
+  const ret1 = await registration.execute("twitter", "aaa reg tipgame2jsvqktwwx2txppudvxt2v8dtdfvxdbrhg", "1234567890")
+  console.log(ret1)
+
+  const client = await apiClient.createWSClient(RPC_ENDPOINT);
+  try {
+
+  } finally {
+    await client.disconnect()
+  }
+  const ret2 = await client.invoke('tiplsk:pendingTx');
+  console.log(ret2)
+})()
