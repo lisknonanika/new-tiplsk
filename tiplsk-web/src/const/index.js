c60c4a58c8b0863f6936e5e05c0c307570080aa8
lisknonanika <lisknonanika@gmail.com>
2021-06-11 15:24:22 +0900

    upd

--- tiplsk-web/src/const/index.js
+++ tiplsk-web/src/const/index.js
@@ -1,4 +1,4 @@
-const RPC_ENDPOINT = "ws://172.23.12.220:8080/ws";
+const RPC_ENDPOINT = "ws://127.0.0.1:8080/ws";
 const URL = [
   {type: "twitter", prof: "https://twitter.com/intent/user?user_id="}
 ];
